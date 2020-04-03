48a3390827205228f0f8836843b6e08ec389cd99
Simon Podlipsky <simon@podlipsky.net>
2020-04-03 20:46:49 +0200

    is-a

--- src/Type/WebMozartAssert/AssertTypeSpecifyingExtension.php
+++ src/Type/WebMozartAssert/AssertTypeSpecifyingExtension.php
@@ -371,6 +371,18 @@ class AssertTypeSpecifyingExtension implements StaticMethodTypeSpecifyingExtensi
 						]
 					);
 				},
+
+				'isAOf' => static function (Scope $scope, Arg $expr, Arg $class): ?\PhpParser\Node\Expr {
+					$classType = $scope->getType($class->value);
+					if (!$classType instanceof ConstantStringType) {
+						return null;
+					}
+
+					return new \PhpParser\Node\Expr\Instanceof_(
+						$expr->value,
+						new \PhpParser\Node\Name($classType->getValue())
+					);
+				},
 			];
 		}
 
